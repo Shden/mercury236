f9155419e5210789e7dc54d87793dd909323143f
den <den@dataart.com>
2014-04-17 00:34:08 +0400

    file desc

--- mercury236.c
+++ mercury236.c
@@ -1,3 +1,9 @@
+/*
+ *	Mercury 236 power meter communication utility.
+ *
+ *	RS485 USB dongle is used to connect to the power meter and to collect grid power measures
+ *	including voltage, current, consumption power, counters, cos(f) etc.
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/select.h>
